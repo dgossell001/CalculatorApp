fa93f0a172ab20fb2f6c35b3cdb9725511b510fa
dgossell001 <itpro@gossellmn.com>
2017-02-24 22:21:19 -0600

    Keypad layout and event handler for number and decimal keys.

--- CalculatorApp/CalculatorApp/xamlCalculator.xaml.cs
+++ CalculatorApp/CalculatorApp/xamlCalculator.xaml.cs
@@ -14,5 +14,42 @@ namespace CalculatorApp
         {
             InitializeComponent();
         }
+
+        void OnNumberButtonClick(object sender, EventArgs args)
+        {
+            // Add the digit pushed to the right-most position of the display
+            Button btn = (Button)sender;
+
+            string strNumberName = btn.ToString().Substring(0,3);
+            string strNumberDigit = null;
+
+            if (btn == oneButton) { strNumberDigit = "1"; }
+            if (btn == twoButton) { strNumberDigit = "2"; }
+            if (btn == thrButton) { strNumberDigit = "3"; }
+            if (btn == fouButton) { strNumberDigit = "4"; }
+            if (btn == fivButton) { strNumberDigit = "5"; }
+            if (btn == sixButton) { strNumberDigit = "6"; }
+            if (btn == sevButton) { strNumberDigit = "7"; }
+            if (btn == eigButton) { strNumberDigit = "8"; }
+            if (btn == ninButton) { strNumberDigit = "9"; }
+            if (btn == zerButton) { strNumberDigit = "0"; }
+            if (btn == decButton) { strNumberDigit = "."; }
+
+            string strDisplay = displayLabel.Text.Trim();
+
+            // if the display is already zero, dump it so we don't end up with a leading zero
+            if (strDisplay == "0") { strDisplay = ""; }
+
+            if(strDisplay.Length < 15)
+            {
+                if(strNumberDigit != "." || strDisplay.IndexOf(".") == 0 )
+                {
+                    strDisplay += strNumberDigit.Trim();
+                }
+                
+            }
+
+            displayLabel.Text = strDisplay;
+        }
     }
 }
